ade411952895b37f42119de13cee9d03267d4392
prakashkukanoor <prakashkop054@gmail.com>
2025-04-29 16:54:57 -0400

    root module setup

--- variables.tf
+++ variables.tf
@@ -1,9 +1,10 @@
 variable "applications" {
   type = map(object({
-    buckets                   = list(string)
-    dynamodb_tables           = list(string)
+    buckets                        = list(string)
+    dynamodb_tables                = list(string)
     dynamo_db_policy_json_tpl_path = string
-    s3_policy_json_tpl_path = string
+    s3_policy_json_tpl_path        = string
+    arn                            = string
   }))
 }
 
