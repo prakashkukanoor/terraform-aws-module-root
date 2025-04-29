2ec9bfcba85028b2ecc42d3d1f6b2497580c7804
prakashkukanoor <prakashkop054@gmail.com>
2025-04-29 10:24:50 -0400

    root module setup

--- main.tf
+++ main.tf
@@ -1,4 +1,8 @@
 module "s3" {
   source = "git@github.com:prakashkukanoor/terraform-aws-s3-module.git?ref=v1.0.0"
 
+  applications = var.applications
+  environment  = var.environment
+  team         = var.team
+
 }
