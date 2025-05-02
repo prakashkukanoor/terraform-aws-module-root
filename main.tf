83a8c698720720bc156b4edb0dbb063b04467f8c
prakashkukanoor <prakashkop054@gmail.com>
2025-05-02 13:39:23 -0400

    root module setup

--- main.tf
+++ main.tf
@@ -1,3 +1,20 @@
+module "networking" {
+  source = "git@github.com:prakashkukanoor/terraform-aws-vpc-subnets-routetable.git?ref=v1.0.5"
+
+  applications                = var.applications
+  environment                 = var.environment
+  team                        = var.team
+  region                      = var.region
+  vpc_cidr_ipv4               = var.vpc_cidr_ipv4
+  enable_ipv6                 = var.enable_ipv6
+  availability_zone           = var.availability_zone
+  application_public_subnets  = var.application_public_subnets
+  application_private_subnets = var.application_private_subnets
+  database_private_subnets    = var.database_private_subnets
+  vpc_gateway_endpoints       = var.vpc_gateway_endpoints
+  vpc_interface_endpoints     = var.vpc_interface_endpoints
+
+}
 module "s3" {
   source = "git@github.com:prakashkukanoor/terraform-aws-s3-module.git?ref=v1.0.1"
 
@@ -5,6 +22,8 @@ module "s3" {
   environment  = var.environment
   team         = var.team
   region       = var.region
+
+   depends_on = [module.networking]
 
 }
 
@@ -15,5 +34,7 @@ module "dynamodb" {
   environment  = var.environment
   team         = var.team
   region       = var.region
+
+  depends_on = [module.networking]
 
 }
