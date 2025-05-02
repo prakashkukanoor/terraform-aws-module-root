95d633367d363907cd9cab3fc7b8e483d766bbc2
prakashkukanoor <prakashkop054@gmail.com>
2025-05-02 13:44:35 -0400

    root module setup

--- variables.tf
+++ variables.tf
@@ -18,3 +18,51 @@ variable "team" {
 variable "region" {
   type = string
 }
+
+variable "enable_ipv6" {
+  type = bool
+}
+
+variable "vpc_cidr_ipv4" {
+  type = string
+}
+
+variable "availability_zone" {
+  type = list(string)
+}
+
+variable "application_public_subnets" {
+  type = list(object({
+    ipv4_cidr  = string
+    ipv6_index = number
+  }))
+}
+
+variable "application_private_subnets" {
+  type = list(object({
+    ipv4_cidr  = string
+    ipv6_index = number
+  }))
+}
+
+variable "database_private_subnets" {
+  type = list(object({
+    ipv4_cidr  = string
+    ipv6_index = number
+  }))
+}
+
+variable "vpc_gateway_endpoints" {
+  type = map(bool)
+  default = {
+    s3       = true
+    dynamodb = true
+  }
+}
+
+variable "vpc_interface_endpoints" {
+  type = map(bool)
+  default = {
+    events = true
+  }
+}
