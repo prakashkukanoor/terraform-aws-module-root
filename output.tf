2feb1b432c65dd9c937aebf49578bdbebb10760a
prakashkukanoor <prakashkop054@gmail.com>
2025-05-05 14:43:18 -0400

    root module setup

---
+++ output.tf
@@ -0,0 +1,23 @@
+output "application_public_subnet_ids" {
+  value = module.networking.application_public_subnet_ids
+}
+
+# output "application_private_subnet_ids" {
+#   value = aws_subnet.application_private[*].id
+# }
+
+# output "database_private_subnet_ids" {
+#   value = aws_subnet.database_private[*].id
+# }
+
+# output "aws_vpce_gateway_dynamodb_id" {
+#   value = aws_vpc_endpoint.gateway["dynamodb"].id
+# }
+
+# output "aws_vpce_gateway_s3_id" {
+#   value = aws_vpc_endpoint.gateway["s3"].id
+# }
+
+# output "aws_vpce_interface_events_id" {
+#   value = aws_vpc_endpoint.interface["events"].id
+# }
