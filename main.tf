0d40f5a8ceb1906dfd1b383631d2e4f928e83301
prakashkukanoor <prakashkop054@gmail.com>
2025-12-26 08:03:51 -0500

    remove networking

--- main.tf
+++ main.tf
@@ -1,19 +1,19 @@
-module "networking" {
-  source = "git@github.com:prakashkukanoor/terraform-aws-vpc-subnets-routetable.git?ref=v1.0.5"
+# module "networking" {
+#   source = "git@github.com:prakashkukanoor/terraform-aws-vpc-subnets-routetable.git?ref=v1.0.5"
 
-  environment                 = var.environment
-  team                        = var.team
-  region                      = var.region
-  vpc_cidr_ipv4               = var.vpc_cidr_ipv4
-  enable_ipv6                 = var.enable_ipv6
-  availability_zone           = var.availability_zone
-  application_public_subnets  = var.application_public_subnets
-  application_private_subnets = var.application_private_subnets
-  database_private_subnets    = var.database_private_subnets
-  vpc_gateway_endpoints       = var.vpc_gateway_endpoints
-  vpc_interface_endpoints     = var.vpc_interface_endpoints
+#   environment                 = var.environment
+#   team                        = var.team
+#   region                      = var.region
+#   vpc_cidr_ipv4               = var.vpc_cidr_ipv4
+#   enable_ipv6                 = var.enable_ipv6
+#   availability_zone           = var.availability_zone
+#   application_public_subnets  = var.application_public_subnets
+#   application_private_subnets = var.application_private_subnets
+#   database_private_subnets    = var.database_private_subnets
+#   vpc_gateway_endpoints       = var.vpc_gateway_endpoints
+#   vpc_interface_endpoints     = var.vpc_interface_endpoints
 
-}
+# }
 module "s3" {
   source = "git@github.com:prakashkukanoor/terraform-aws-s3-module.git?ref=v1.0.1"
 
