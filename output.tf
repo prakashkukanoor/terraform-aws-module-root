8f30aca79066de1bf97046947842a6b52e9ec4c9
prakashkukanoor <prakashkop054@gmail.com>
2026-07-11 11:09:25 -0400

    updating eks outputs

--- output.tf
+++ output.tf
@@ -1,15 +1,20 @@
 output "eks_cluster_endpoint" {
-  description = "Endpoint for EKS control plane"
+  description = "EKS cluster endpoint"
   value       = module.eks.cluster_endpoint
 }
 
+output "cluster_name" {
+  description = "EKS cluster name"
+  value       = module.eks.cluster_name
+}
+
 output "eks_cluster_security_group_id" {
-  description = "Endpoint for EKS control plane"
+  description = "EKS cluster Security group ID"
   value       = module.eks.cluster_security_group_id
 }
 
 output "eks_cluster_iam_role_name" {
-  description = "Endpoint for EKS control plane"
+  description = "EKS cluster IAM role"
   value       = module.eks.cluster_iam_role_name
 }
 
@@ -19,8 +24,13 @@ output "eks_cluster_iam_role_name" {
 # }
 
 output "eks_node_group_name" {
-  description = "Base64 encoded certificate data required to communicate with the cluster"
+  description = "EKS node group"
   value       = module.eks.node_group_name
+}
+
+output "aws_autoscaling_group_id" {
+  description = "EKS node group Name"
+  value       = module.eks.aws_autoscaling_group_id
 }
 
 output "eks_configure_kubectl" {
